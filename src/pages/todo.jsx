9ebc0da886440c5d327459b8ef27c8848c6511c8
hasedai0000 <hasegawa.daichi.workman@gmail.com>
2024-06-02 16:02:43 +0900

    【Atomic】まずはテンプレート化

--- src/pages/todo.jsx
+++ src/pages/todo.jsx
@@ -3,95 +3,11 @@
  *
  * @package pages
  */
-import styles from "./styles.module.css";
-import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
-import { INIT_TODO_LIST, INIT_UNIQUE_ID } from "../constants/data.js";
+import { TodoTemplate } from "../components/templates/TodoTemplate";
 
 /**
  * TodoPage
  * @returns {JSX.Element}
  * @constructor
  */
-export const TodoPage = () => {
-  /* todo list */
-  const [originTodoList, setOriginTodoList] = useState(INIT_TODO_LIST);
-  /* add input title */
-  const [addInputValue, setAddInputValue] = useState("");
-  /* todo 採番ID */
-  const [uniqueId, setUniqueId] = useState(INIT_UNIQUE_ID);
-  /* search input title */
-  const [searchInputValue, setSearchInputValue] = useState("");
-
-  const onChangeAddInputValue = (e) => {
-    setAddInputValue(e.target.value);
-  };
-
-  const onChangeSearchInputValue = (e) => {
-    setSearchInputValue(e.target.value);
-  };
-
-  const handleAddTodo = (e) => {
-    if (e.key === "Enter" && addInputValue !== "") {
-      const nextUniqueId = uniqueId + 1;
-      const newTodoList = [
-        ...originTodoList,
-        {
-          id: nextUniqueId,
-          title: addInputValue,
-        },
-      ];
-      setOriginTodoList(newTodoList);
-      // 採番IDを更新
-      setUniqueId(nextUniqueId);
-      // todo追加後、入力値をリセット
-      setAddInputValue("");
-    }
-  };
-
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Todo List</h1>
-      <section className={styles.common}>
-        <h2 className={styles.subTitle}>ADD TODO</h2>
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="New Todo"
-          value={addInputValue}
-          onChange={onChangeAddInputValue}
-          onKeyDown={handleAddTodo}
-        />
-      </section>
-      <section className={styles.common}>
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="Search Keyword"
-          value={searchInputValue}
-          onChange={onChangeSearchInputValue}
-        />
-      </section>
-      <section className={styles.common}>
-        <ul className={styles.list}>
-          {originTodoList
-            .filter((todo) => {
-              const isMatch = todo.title.indexOf(searchInputValue) !== -1;
-              return isMatch;
-            })
-            .map((todo) => {
-              return (
-                <li className={styles.todo} key={todo.id}>
-                  <span className={styles.task}>{todo.title}</span>
-                  <div className={styles.far}>
-                    <FontAwesomeIcon icon={faTrashAlt} size="lg" />
-                  </div>
-                </li>
-              );
-            })}
-        </ul>
-      </section>
-    </div>
-  );
-};
+export const TodoPage = () => <TodoTemplate />;
