d4fd3fce8ff8be4837cdc8485203c34778dc8069
hasedai0000 <hasegawa.daichi.workman@gmail.com>
2024-06-04 00:22:27 +0900

    【Atomic】organisms化

--- src/components/templates/TodoTemplate/index.jsx
+++ src/components/templates/TodoTemplate/index.jsx
@@ -4,10 +4,10 @@
  * @package pages
  */
 import styles from "./styles.module.css";
-import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import { INIT_TODO_LIST, INIT_UNIQUE_ID } from "../../../constants/data.js";
+import { AddTodo } from "../../organisms/AddTodo/index.jsx";
+import { TodoList } from "../../organisms/TodoList/index.jsx";
 
 /**
  * TodoPage
@@ -72,14 +72,10 @@ export const TodoTemplate = () => {
       <h1 className={styles.title}>Todo List</h1>
       {/* タスク入力画面 */}
       <section className={styles.common}>
-        <h2 className={styles.subTitle}>ADD TODO</h2>
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="New Todo"
-          value={addInputValue}
-          onChange={onChangeAddInputValue}
-          onKeyDown={handleAddTodo}
+        <AddTodo
+          addInputValue={addInputValue}
+          onChangeAddInputValue={onChangeAddInputValue}
+          handleAddTodo={handleAddTodo}
         />
       </section>
       {/* タスク絞り込み画面 */}
@@ -94,27 +90,13 @@ export const TodoTemplate = () => {
       </section>
       {/* タスク一覧表示画面 */}
       <section className={styles.common}>
-        <ul className={styles.list}>
-          {originTodoList
-            .filter((todo) => {
-              const isMatch = todo.title.indexOf(searchInputValue) !== -1;
-              return isMatch;
-            })
-            .map((todo) => {
-              return (
-                <li className={styles.todo} key={todo.id}>
-                  <span className={styles.task}>{todo.title}</span>
-                  <div className={styles.far}>
-                    <FontAwesomeIcon
-                      icon={faTrashAlt}
-                      size="lg"
-                      onClick={() => handleDeleteTodo(todo.id, todo.title)}
-                    />
-                  </div>
-                </li>
-              );
-            })}
-        </ul>
+        {originTodoList.length > 0 && (
+          <TodoList
+            originTodoList={originTodoList}
+            searchInputValue={searchInputValue}
+            handleDeleteTodo={handleDeleteTodo}
+          />
+        )}
       </section>
     </div>
   );
